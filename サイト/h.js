57aa1a134fc90a6640cd8bf3f2273ab01d4ed66d
tkun1212 <148056002+tkun1212@users.noreply.github.com>
2023-11-05 13:53:03 +0900

    Add files via upload

---
+++ サイト/h.js
@@ -0,0 +1,17 @@
+function first(){
+    var user = prompt("ユーザー名を入力してね");
+        if(user=="000A0001" || user=="000A0002" || user=="000A0003"){
+        var pass=prompt("パスワードを入力してね！！")
+var passWord="S.T.A fast project"
+if(pass == passWord){
+    alert("正解！！");
+}else{
+    alert("ユーザー名又はパスワードが間違ってます。");
+    first();
+}
+    }else{
+    alert=("ユーザー名を入力してください。");
+    first();
+    }
+}
+    window.onload=first
